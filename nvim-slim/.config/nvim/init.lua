b69a6f1dd2397762492e2f60ef49260ddad36045
Miniś <domin.mikol@gmail.com>
2025-06-05 14:39:18 +0200

    nowy moj od zera config nvim-slim (jeszcze w trakcie)

---
+++ nvim-slim/.config/nvim/init.lua
@@ -0,0 +1,26 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none", "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("options")
+
+-- Jawne ładowanie pluginów
+local plugins = {
+  require("plugins.oneliners"),
+  require("plugins.catppuccin"),
+  require("plugins.telescope"),
+  require("plugins.treesitter"),
+  require("plugins.neo-tree"),
+}
+
+require("lazy").setup(plugins, {
+  install = { colorscheme = { "catppuccin" } },
+  checker = { enabled = true },
+})
+
