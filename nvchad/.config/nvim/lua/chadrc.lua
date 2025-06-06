4bd91879aedffb08896fb4ae8a6bc54712247576
Miniś <domin.mikol@gmail.com>
2025-06-06 14:23:05 +0200

    konfiguracja nvchad, bo ładne motywy itp out of the box

---
+++ nvchad/.config/nvim/lua/chadrc.lua
@@ -0,0 +1,28 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "onedark",
+  transparency = true,
+}
+
+-- M.nvdash = { load_on_startup = true }
+M.ui = {
+  telescope = {style = "bordered"},
+}
+
+M.term = {
+  winopts = { number = false },
+  sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+  float = {
+     row = 0.3, col = 0.25,
+     width = 0.5, height = 0.4,
+     border = "single",
+  },
+}
+
+return M
