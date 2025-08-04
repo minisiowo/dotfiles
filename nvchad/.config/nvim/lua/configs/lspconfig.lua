40d3f01a77547d9ea96426d6b0035a614c0cfe3c
Minis <domin.mikol@gmail.com>
2025-08-04 20:01:08 +0200

    czyścimi, neovim w osobnym repo jak narazie, nowy .gitignore itd itd

--- nvchad/.config/nvim/lua/configs/lspconfig.lua
+++
@@ -1,6 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "lua_ls", "html", "cssls", "pyright" }
-vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
