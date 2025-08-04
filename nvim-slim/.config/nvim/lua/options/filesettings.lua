986b12a6d648348af8a435f3555d3b8d308f695c
Minis <domin.mikol@gmail.com>
2025-08-04 20:01:08 +0200

    czyścimi, neovim w osobnym repo jak narazie, nowy .gitignore itd itd

--- nvim-slim/.config/nvim/lua/options/filesettings.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "lua",
-    callback = function()
-        -- czyli tylko dla bieżącego bufora
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-    end,
-})
