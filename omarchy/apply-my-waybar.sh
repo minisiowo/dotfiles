c588eb2da34e7de94b6024aac4c19ef789926a07
Dominik Mikołajczyk <domin.mikol@gmail.com>
2026-02-02 16:55:54 +0100

    apply-my-waybar.sh fixes

--- omarchy/apply-my-waybar.sh
+++ omarchy/apply-my-waybar.sh
@@ -10,7 +10,7 @@ jq '
   ."modules-center" = ["hyprland/workspaces", "custom/update", "custom/voxtype", "custom/screenrecording-indicator"] |
   ."modules-right" = ["group/tray-expander", "bluetooth", "network", "pulseaudio", "cpu", "battery", "custom/omarchy"] |
   ."custom/omarchy".format = "💀" |
-  ."hyprland/workspaces"."format-icons"."9" = ""
+  ."hyprland/workspaces"."format-icons"."10" = ""
 ' "$CONFIG_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$CONFIG_FILE"
 
 omarchy-restart-waybar
