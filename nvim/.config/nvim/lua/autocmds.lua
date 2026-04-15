ef5920a501d2cd94ab051ba528b24424847d0d02
Dominik Mikolajczyk <domin.mikol@gmail.com>
2026-04-15 20:09:39 +0200

    just simple commit

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -25,10 +25,10 @@ vim.filetype.add({
 
 -- no auto continue comments on new line
 vim.api.nvim_create_autocmd("FileType", {
-	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
-	callback = function()
-		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-	end,
+    group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+    callback = function()
+        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+    end,
 })
 
 -- Auto save when leaving buffer, losing focus, or leaving insert mode
